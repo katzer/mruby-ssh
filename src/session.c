8ef860c5bb8822e4910af98434b0fcf7b5f4c209
Sebastián Katzer <katzer.sebastian@googlemail.com>
2018-04-27 16:45:19 +0200

    Char might be bigger then 1 byte

--- src/session.c
+++ src/session.c
@@ -296,7 +296,7 @@ mrb_ssh_f_login (mrb_state *mrb, mrb_value self)
     mrb_get_args(mrb, "s|s!?bbs!", &user, &user_len, &pass, &pass_len, &pass_given, &prompt, &pass_is_key, &phrase, &phrase_len);
 
     if (pass_is_key && pass_given) {
-        pubkey = (char*) malloc(pass_len + 4 + 1);
+        pubkey = (char *)malloc(sizeof(char) * (pass_len + 4 + 1));
         strcpy(pubkey, pass);
         strncat(pubkey, ".pub", 4);
     }
